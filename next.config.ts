8df5cb65fd1d2f36d618e7b625e39ad8a9ad52fb
John <psg6868@yahoo.com>
2025-07-25 17:39:30 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,16 @@ import { createMDX } from 'fumadocs-mdx/next';
 const withMDX = createMDX();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withMDX(nextConfig);
